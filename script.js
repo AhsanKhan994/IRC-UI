8155784dbd69747cb319aaa4f32c215383ad42af
AhsanKhan YousafZai <AhsanKhan YousafZai>
2015-12-09 20:22:55 +0500

    Script functions added

---
+++ script.js
@@ -0,0 +1,67 @@
+﻿
+function newConnection() //After login Authentication user will join the all channels that will be present in array list
+{
+	JoinChannels();
+}
+
+function JoinChannels() //Channels will join from here
+{
+	
+}
+
+function kickHimeOutCommand(inp) //Kickout this person
+{
+	
+}
+
+function pingSelectedNickCommand(inp) //ping to this user
+{
+	
+}
+
+function privateMsgSelectedNickCommand(inp) //Start private message with user
+{
+	
+}
+function closeActiveWindowCommand()//Close channel messages window
+{
+	
+}
+function closePrivateMsgCommand()//Close private messages window
+{
+	
+}
+function blockSelectedNickCommand(inp)//block this user
+{
+	
+}
+function changeNickCommand(inp)//Change this user nick
+{
+	
+}
+function joinChannelCommand(inp)//Join this channel
+{
+	
+}function kickSelectedNickCommand(inp)//Kick this user
+{
+	
+}function banSelectedNickCommand(inp)//Ban this user
+{
+	
+}
+function whoisSelectedNickCommand(inp)//whois this user
+{
+	
+}
+function inviteSelectedNickCommand(inp)//Invite this user
+{
+	
+}
+function unmuteThis(user)//unmute user
+{
+	
+}
+function muteThis(user)//mute user
+{
+	
+}
